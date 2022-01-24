47ff502e2ec1efac6ceddf2451fc96df4b9e6c2e
lucasfarmwald <farmwaldlucas@gmail.com>
2022-01-24 12:23:37 -0600

    adding function to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,10 @@
 import react from "react"
  
 function Header(){
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    function handleDarkModeClick() {
-        
-        setIsDarkMode((isDarkMode) => !isDarkMode);
-      }
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-    </div>
-  )}
+  return(
+    console.log('hello')
+  )
+}
 
 
   export default Header
