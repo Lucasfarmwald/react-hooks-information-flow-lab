8883ef63ae0e0a482f171f7702492775236d0cf7
lucasfarmwald <farmwaldlucas@gmail.com>
2022-01-24 12:05:56 -0600

    created header component

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,15 @@ function App() {
   }
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+   // <div className={"App " + (isDarkMode ? "dark" : "light")}>
+     // <header>
+       // <h2>Shopster</h2>
+        //<button onClick={handleDarkModeClick}>
+          //{isDarkMode ? "Dark" : "Light"} Mode
+        //</button>
+      //</header>
       <ShoppingList items={itemData} />
-    </div>
+    //</div>
   );
 }
 
